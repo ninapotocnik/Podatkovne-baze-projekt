37a0073ebe4fb2b65ccef3d8d854d8be8f9b85a3
Nina Potočnik <nina.potocnik@student.fmf.lj-uni.si>
2014-12-15 11:47:43 +0100

    Model

--- festivalskoPoletje.py
+++ festivalskoPoletje.py
@@ -3,19 +3,21 @@ import sqlite3
 datoteka_baze = 'festivalskoPoletje.sqlite3'
 
 def iskanje(ime, lokacija, min_cena, max_cena, min_datum, max_datum):
-    c = baza.cursor()
-    c.execute('''SELECT id, ime FROM festival JOIN nastopi ON festival.id = id_festival
-              JOIN glasbeniki ON id_glasbenik = glasbeniki.id
-              WHERE (festival.ime LIKE ? OR glasbeniki.ime LIKE ?)
-              AND lokacija LIKE ?
-              AND cena IS BETWEEN ? AND ?
-              AND datum IS BETWEEN ? AND ?''', [ime, ime, lokacija, min_cena, max_cena, min_datum, max_datum])
-    ime = c.fetchall()
-    c.close()
-    return ime
+    with baza:
+        baza.execute('''SELECT id, ime FROM festival JOIN nastopi ON festival.id = id_festival
+                  JOIN glasbeniki ON id_glasbenik = glasbeniki.id
+                  WHERE (festival.ime LIKE ? OR glasbeniki.ime LIKE ?)
+                  AND lokacija LIKE ?
+                  AND cena IS BETWEEN ? AND ?
+                  AND datum IS BETWEEN ? AND ?''', [ime, ime, lokacija, min_cena, max_cena, min_datum, max_datum])
+        ime = baza.fetchall()
+        return ime
 
-def vstopnice():
-    c = baza.cursor()
-    c.execute('''SELECT id FROM festival ''')
-    c.close()
+def izdaj_vstopnico_festival(id_festivala, kolicina, popust=0):
+    with baza:
+        baza.execute('''SELECT cena, stevilo_vstopnic FROM festival WHERE id = ?
+                  ''', [id_festivala])
+        return id
+
+    
 baza = sqlite3.connect(datoteka_baze, isolation_level = None)
