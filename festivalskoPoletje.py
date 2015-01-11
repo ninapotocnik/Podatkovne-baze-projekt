8dd1ca38033e3d2e215d275fcced5db48380da02
ninapotocnik <nina.potocnik@student.fmf.uni-lj.si>
2015-01-11 15:26:53 +0100

    model + baza
    Dopolnitev modela in baze.

--- festivalskoPoletje.py
+++ festivalskoPoletje.py
@@ -1,12 +1,15 @@
 import sqlite3
 import hashlib
 
+# iskanje??
+
 datoteka_baze = 'festivalskoPoletje.sqlite3'
 
 def iskanje(ime = '', lokacija = '', min_cena = 30, max_cena = 1000, min_datumZ = '1.5.2014', max_datumZ = '1.9.2014', min_datumK = '3.5.2014', max_datumK = '30.9.2014'):
+    '''Poišče festival glede na podane zahteve.'''
     cur = baza.cursor()
     cur.execute('''SELECT festival.id, festival.ime FROM festival JOIN nastopi ON festival.id = nastopi.id_festival
-              JOIN glasbeniki ON nastopi.id_glasbenik = id
+              JOIN glasbeniki ON nastopi.id_glasbenik = glasbenik.id
               WHERE (festival.ime LIKE ? OR glasbeniki.glasbenik LIKE ?)
               AND festival.lokacija LIKE ?
               AND festival.cena BETWEEN ? AND ?
@@ -18,6 +21,7 @@ def iskanje(ime = '', lokacija = '', min_cena = 30, max_cena = 1000, min_datumZ 
     return rezultat_iskanja
 
 def izdaj_vstopnico_festival(id_festival, kolicina, popust = 0):
+    '''Vstopnica za festival.'''
     # popust je v odstotkih
     cur = baza.cursor()
     cur.execute('''SELECT stevilo_vstopnic, st_prodanih_festival FROM festival WHERE id = ?
@@ -82,11 +86,12 @@ def izdaj_vstopnico_festival(id_festival, kolicina, popust = 0):
         cur.execute('''INSERT INTO Vstopnice (id_festivala, ime_festivala, kolicina, cena_prej, cena_skupaj, popust)
                         VALUES (?, ?, ?, ?, ?, ?)''', [id_festival, ime_festivala, kolicina, cena, cena_skupaj, popust])
         # prištejemo prodane vstopnice
-        cur.execute('''UPDATE festival SET st_prodanih_festival = ?''', [st_prodanih_festival + kolicina])
+        cur.execute('''UPDATE festival SET st_prodanih_festival = ? WHERE id = ?''', [st_prodanih_festival + kolicina, id_festival])
     cur.close()
     return id
 
 def izdaj_vstopnico_nastop(id_nastopa, kolicina, popust = 0):
+    '''Vstopnica za nastop.'''
     # popust je v odstotkih
     cur = baza.cursor()
     # prodane vstopnice in id festivala, da lahko dobimo koliko je vseh vstopnic
@@ -133,16 +138,17 @@ def izdaj_vstopnico_nastop(id_nastopa, kolicina, popust = 0):
         cur.execute('''INSERT INTO vstopnice_nastopi (cena_prej, popust, cena_skupaj, id_nastop, stevilo)
                         VALUES (?, ?, ?, ?, ?)''', [cena, popust, cena_skupaj, id_nastopa, kolicina])
         # prištejejo se kupljene vstopnice
-        cur.execute('''UPDATE nastopi SET st_prodanih_nastop = ?''', [st_prodanih_nastop + kolicina])
+        cur.execute('''UPDATE nastopi SET st_prodanih_nastop = ? WHERE id = ?''', [st_prodanih_nastop + kolicina, id_nastopa])
     cur.close()
     return id
 
 
 def zakodiraj(geslo):
+    '''Zakodiramo dano geslo.'''
     return hashlib.md5(geslo.encode()).hexdigest()
 
 def preveri_geslo(uporabnisko_ime, geslo):
-    '''Preveri če je v bazi uporabnik z podanim uporabniškim imenom in podanim geslom'''
+    '''Preverimo, če je v bazi uporabnik s podanim uporabniškim imenom in geslom'''
     with baza:
         baza.execute('''SELECT id FROM uporabniki WHERE uporabnisko_ime = ? AND geslo = ?''', [uporabnisko_ime, zakodiraj(geslo)])
         id_uporabnika = baza.fetchone()
@@ -150,14 +156,16 @@ def preveri_geslo(uporabnisko_ime, geslo):
             raise Exception('Vnešeno uporabniško ime ali geslo je napačno.')
         
 def dodaj_uporabnika(uporabnisko_ime, geslo):
-    '''V bazo dodamo uporabnika: njegovo uporabniško ime in zakodirano geslo'''
+    '''V bazo dodamo uporabnika, to pomeni njegovo uporabniško ime in zakodirano geslo'''
     cur = baza.cursor()
     cur.execute('''INSERT INTO uporabniki (uporabnisko_ime, geslo) VALUES (?, ?)''', [uporabnisko_ime, zakodiraj(geslo)])
     cur.close()
-    id_uporabnika = cur.lastrowid #dobi zadnji id
+    # dobi zadnji id
+    id_uporabnika = cur.lastrowid 
     return id_uporabnika
 
 def dodaj_komentar(komentar, uporabnisko_ime):
+    '''V bazo dodamo komentar.'''
     cur = baza.cursor()
     cur.execute('''INSERT INTO komentarji (komentar, uporabnisko_ime) VALUES (?, ?)''', [komentar, uporabnisko_ime])
     cur.close()
